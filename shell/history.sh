7ddc3e05dcacf08de8f4fa807b6b0e79208c8341
Szynkaa <szymon.ant@hotmail.com>
2025-01-24 20:51:45 +0100

    Update history related shell variables values

--- shell/history.sh
+++ shell/history.sh
@@ -1,5 +1,5 @@
 export HISTCONTROL="ignoreboth:erasedups"
-export HISTSIZE=5000
-export HISTFILESIZE=5000
-export HISTIGNORE="ls:ll:l:clear:cd:history"
+export HISTSIZE=10000
+export HISTFILESIZE=10000
+export HISTIGNORE="ls:ll:l:clear:cd:history:*.venv/bin/activate"
 
